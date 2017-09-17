d5c32f99b31934be5da86ea222b3ae6d8cc78d5f
Patrick Houlihan <pathouli@publicisgroupe.net>
2017-09-17 14:30:32 -0400

    add

---
+++ class/classEx/class1a.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 15:36:39 2017
+
+@author: pathouli
+"""
+
+#!/usr/bin/python                                                               
+import sys
+freq = {}   # frequency of words in text                                        
+for line in sys.stdin:
+    for word in line.split():
+        if word in freq:
+            freq[word] = 1 + freq[word]
+        else:
+            freq[word] = 1
+print freq
+
+#make a chage
